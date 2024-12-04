4cf0fc997795cc8aca011da1b7689c3ea94b3e72
Alejandro <alejandrolujan1999@outlook.es>
2024-12-04 19:25:01 +0000

    Instagram

--- src/models.py
+++ src/models.py
@@ -7,29 +7,6 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-'''
-
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-        def to_dict(self):
-        return {}
-'''
-
 class Follower(Base):
     __tablename__ = 'follower'
     id = Column(Integer, primary_key=True)
@@ -48,22 +25,20 @@ class Comment(Base):
     __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
     comment_text = Column(String(15))
-    author_id = Column(Integer)
-    post_id = Column(Integer)
+    author_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
 
 class Post(Base):
     __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
 
 class Media(Base):
     __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
     type = Column(Enum)
     url = Column(String(15))
-    post_id = Column(Integer)
-
-
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
 
 ## Draw from SQLAlchemy base
 try:
